496abfd843f86b0085e592d7c61d630ae6cfe79f
jamesu <jamesu@1e8d30c7-b1cd-4b0d-952e-12ddc4cacc65>
2007-11-10 19:16:49 +0000

    - User avatar file is now deleted when updating - Company logo file is now deleted when updating
    git-svn-id: svn+ssh://rubyforge.org/var/svn/railscollab/trunk@118 1e8d30c7-b1cd-4b0d-952e-12ddc4cacc65

--- app/models/company.rb
+++ app/models/company.rb
@@ -1,7 +1,22 @@
 =begin
 RailsCollab
 -----------
-Copyright (C) 2007 James S Urquhart (jamesu at gmail.com)This program is free software; you can redistribute it and/ormodify it under the terms of the GNU General Public Licenseas published by the Free Software Foundation; either version 2of the License, or (at your option) any later version.This program is distributed in the hope that it will be useful,but WITHOUT ANY WARRANTY; without even the implied warranty ofMERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See theGNU General Public License for more details.You should have received a copy of the GNU General Public Licensealong with this program; if not, write to the Free SoftwareFoundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+
+Copyright (C) 2007 James S Urquhart (jamesu at gmail.com)
+
+This program is free software; you can redistribute it and/or
+modify it under the terms of the GNU General Public License
+as published by the Free Software Foundation; either version 2
+of the License, or (at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program; if not, write to the Free Software
+Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
 =end
 
 require 'gd2'
@@ -79,8 +94,9 @@ class Company < ActiveRecord::Base
 	end
 	
 	def logo=(value)
+		FileRepo.handle_delete(self.logo_file) unless self.logo_file.nil?
+		
 		if value.nil?
-			FileRepo.handle_delete(self.logo_file)
 			self.logo_file = nil
 			return
 		end
