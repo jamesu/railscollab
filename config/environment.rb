52bf763f400782f562986ec7f0391a64398a8c08
jamesu <jamesu@1e8d30c7-b1cd-4b0d-952e-12ddc4cacc65>
2007-12-24 13:41:27 +0000

    - Explicitly state TZ
    git-svn-id: svn+ssh://rubyforge.org/var/svn/railscollab/trunk@233 1e8d30c7-b1cd-4b0d-952e-12ddc4cacc65

--- config/environment.rb
+++ config/environment.rb
@@ -7,6 +7,8 @@
 # Specifies gem version of Rails to use when vendor/rails is not present
 RAILS_GEM_VERSION = '2.0.1' unless defined? RAILS_GEM_VERSION
 
+ENV['TZ'] = 'UTC'
+
 # Bootstrap the Rails environment, frameworks, and default configuration
 require File.join(File.dirname(__FILE__), 'boot')
 
