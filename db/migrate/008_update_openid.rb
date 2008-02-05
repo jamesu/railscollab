5578dbd81fb9627f7610795533b3f8f7821275a9
jamesu <jamesu@1e8d30c7-b1cd-4b0d-952e-12ddc4cacc65>
2008-02-05 09:15:31 +0000

    - Whoops, missed migration 8
    git-svn-id: svn+ssh://rubyforge.org/var/svn/railscollab/trunk@245 1e8d30c7-b1cd-4b0d-952e-12ddc4cacc65

---
+++ db/migrate/008_update_openid.rb
@@ -0,0 +1,43 @@
+class UpdateOpenid < ActiveRecord::Migration
+  def self.up
+    create_table :open_id_authentication_associations, :force => true do |t|
+      t.integer :issued, :lifetime
+      t.string :handle, :assoc_type
+      t.binary :server_url, :secret
+    end
+    
+    create_table :open_id_authentication_nonces, :force => true do |t|
+      t.integer :timestamp, :null => false
+      t.string :server_url, :null => true
+      t.string :salt, :null => false
+    end
+    
+    drop_table "open_id_associations"
+    drop_table "open_id_settings"
+    drop_table "open_id_nonces"
+  end
+
+  def self.down
+    create_table "open_id_associations", :force => true do |t|
+      t.column "server_url", :binary
+      t.column "handle", :string
+      t.column "secret", :binary
+      t.column "issued", :integer
+      t.column "lifetime", :integer
+      t.column "assoc_type", :string
+    end
+
+    create_table "open_id_nonces", :force => true do |t|
+      t.column "nonce", :string
+      t.column "created", :integer
+    end
+
+    create_table "open_id_settings", :force => true do |t|
+      t.column "setting", :string
+      t.column "value", :binary
+    end
+    
+    drop_table "open_id_authentication_associations"
+    drop_table "open_id_authentication_nonces"
+  end
+end
