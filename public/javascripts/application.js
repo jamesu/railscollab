0841b9ce2e4fe7d0fc6e2aab8989ea58c76d0c8d
jamesu <jamesu@1e8d30c7-b1cd-4b0d-952e-12ddc4cacc65>
2008-02-05 13:07:39 +0000

    - User box now uses LowPro
    git-svn-id: svn+ssh://rubyforge.org/var/svn/railscollab/trunk@250 1e8d30c7-b1cd-4b0d-952e-12ddc4cacc65

--- public/javascripts/application.js
+++ public/javascripts/application.js
@@ -12,65 +12,50 @@ var StatusBar = Behavior.create({
 });
 
 // Userbox stuff
-var userbox_menu_names = ['account_more_', 'projects_more_', 'administration_more_'];
-var userbox_current_menu = null;
-    
-function userbox_reset_menus()
-{
-	userbox_menu_names.each(function(el_name){
-		var el = $(el_name + 'menu');
-		if (el && (el != userbox_current_menu))
-		{
-			el.style.display = 'none';
-		}
-	});
-}
 
-function userbox_menu_switch(icon, el)
-{
-	var icon_pos = Position.cumulativeOffset(icon);
+var PopupMenu = Behavior.create({
+	initialize: function()
+	{
+		PopupMenu.current_popup = null;
+	},
 	
-	el.style.left = icon_pos[0] + 'px';
-	el.style.top = (icon_pos[1] + icon.offsetHeight) + 'px';
-	el.style.display = el.style.display == 'none' ? 'block' : 'none';
-	
-	if (el.style.display == 'none')
-		userbox_current_menu = null;
-	else
-		userbox_current_menu = el;
-}
-    
-function userbox_init()
-{
-	var el_names = ['account_more_', 'projects_more_', 'administration_more_'];
-	var el_handlers = {
-		account_more_: function(evt){
-			userbox_menu_switch($('account_more_icon'), $('account_more_menu'));
-			userbox_reset_menus();
-			Event.stop(evt);
-		},
-		projects_more_: function(evt){
-			userbox_menu_switch($('projects_more_icon'), $('projects_more_menu'));
-			userbox_reset_menus();
-			Event.stop(evt);
-		},
-		administration_more_: function(evt){
-			userbox_menu_switch($('administration_more_icon'), $('administration_more_menu'));
-			userbox_reset_menus();
-			Event.stop(evt);
-		}
-	};
-	    
-	// Associate event handlers
-	userbox_menu_names.each(function(el_name){
-		var el = $(el_name + 'menu');
-		if (el)
+	onclick: function(e)
+	{
+		var element = e.element();
+		e.stop();
+		
+		var popup_menu = $(element.identify() + '_menu');
+		if (popup_menu && (popup_menu != PopupMenu.current_popup))
 		{
-			el.style.display = 'none';
-			Event.observe(el_name + 'icon', 'click', el_handlers[el_name], false);
+			var offset = Position.cumulativeOffset(element);
+			offset[1] += element.offsetHeight;
+			this.switch_menu(offset, popup_menu);
 		}
-	});
-}
+		else
+		{
+			this.switch_menu(null, null);
+		}
+	},
+	
+	switch_menu: function(new_pos, new_menu)
+	{
+		if (PopupMenu.current_popup)
+		{
+			PopupMenu.current_popup.style.display = 'none';
+		}
+		
+		PopupMenu.current_popup = new_menu;
+		
+		if (!new_menu)
+			return;
+		
+		new_menu.style.left = new_pos[0] + 'px';
+		new_menu.style.top = new_pos[1] + 'px';
+		new_menu.style.display = 'block';
+	}
+});
+
+// Login form stuff
 
 function login_toggle_openid()
 {
@@ -304,9 +289,8 @@ function notify_form_select_company(id)
 		$('notifyUser' + user_id).checked = true;
 	});
 }
-  	
-Event.observe(window, 'load', userbox_init, false);
 
 Event.addBehavior({
-	'#status_bar' : StatusBar()
+	'#status_bar' : StatusBar(),
+	'.popup_menu_selector' : PopupMenu()
 });
