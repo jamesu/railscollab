348a40af53372adb3aff087a8721f2c35b8ac7ce
jamesu <jamesu@gmail.com>
2008-11-13 10:02:20 +0000

    Files are now migrated in revision 14. Data should now be safe!

--- db/migrate/014_paperclip_files.rb
+++ db/migrate/014_paperclip_files.rb
@@ -6,6 +6,30 @@ class PaperclipFiles < ActiveRecord::Migration
     add_column :project_file_revisions, 'data_updated_at',   :datetime
     add_column :project_file_revisions, 'has_thumbnail',     :boolean, :default => false
     
+    # Migrate the data to the new system
+    ProjectFileRevision.find(:all).each do |file|
+      repo = TempRepo.find(:first, :conditions => {'id' => file.repository_id})
+      unless repo.nil?
+        puts "Migrating file #{file.project_file.filename} (revision #{file.id})"
+        if repo.storage != :database
+          puts "  WARNING: storage type #{repo.storage_id} for file #{repo.file_id} not handled"
+        end
+
+        # Grab data
+        content = ActionController::UploadedTempfile.new("CGI")
+        content.binmode if defined? content.binmode
+        content.print repo.content
+        content.content_type = file.type_string
+        content.original_path = file.project_file.filename
+        content.rewind
+
+        # Store in new system
+        file.upload_file = content
+        file.save
+      end
+    end
+    
+    # Now we can drop the old stuff
     drop_table :file_repo
     remove_column :project_file_revisions, 'repository_id'
     remove_column :project_file_revisions, 'thumb_filename'
@@ -21,10 +45,43 @@ class PaperclipFiles < ActiveRecord::Migration
       t.integer "storage_id", :limit => 1, :default => 0, :null => false
     end
     
+    # Ok, time to migrate data back to old system
+    ProjectFileRevision.find(:all).each do |file|
+      puts "Migrating file #{file.project_file.filename} (revision #{file.id})"
+      
+      repo = TempRepo.new
+      begin
+        File.open(file.data.path, 'rb') do |io|
+          repo.content = io.read
+        end
+      rescue Exception => e
+        puts "  WARNING: couldn't migrate data #{e.to_s}"
+        repo.content = ''
+      end
+      
+      repo.storage_id = 0
+      repo.save
+      
+      file.repository_id = repo.id
+      file.save
+    end
+    
+    
     remove_column :project_file_revisions, "data_file_name"
     remove_column :project_file_revisions, "data_content_type"
     remove_column :project_file_revisions, "data_file_size"
     remove_column :project_file_revisions, 'data_updated_at'
     remove_column :project_file_revisions, 'has_thumbnail'
+  end
+end
+
+class TempRepo < ActiveRecord::Base
+  set_table_name 'file_repo'
+
+  @@storage_lookup = {:database => 0, :s3 => 1}
+  @@storage_id_lookup = @@storage_lookup.invert
+
+  def storage
+    @@storage_id_lookup[self.storage_id]
   end
 end
