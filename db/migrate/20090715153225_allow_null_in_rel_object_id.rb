c8d6a98e44419b1d6d8a29008cf29180143fc75d
Sergio Cambra <sergio@entrecables.com>
2009-07-30 06:09:02 +0800

    Remove adding configuration options in migrations
    Signed-off-by: James Urquhart <jamesu@gmail.com>

--- db/migrate/20090715153225_allow_null_in_rel_object_id.rb
+++ db/migrate/20090715153225_allow_null_in_rel_object_id.rb
@@ -1,13 +1,9 @@
 class AllowNullInRelObjectId < ActiveRecord::Migration
   def self.up
     change_column_null :application_logs, :rel_object_id, true, nil
-    opt = ConfigOption.new(:category_name => 'general', :name => 'log_really_silent', :config_handler_class => 'BoolConfigHandler', :is_system => false, :option_order => 12, :dev_comment => '')
-    opt.handledValue = false
-    opt.save
   end
 
   def self.down
     change_column_null :application_logs, :rel_object_id, false, 0
-    ConfigOption.delete_all :name => 'log_really_silent'
   end
 end
